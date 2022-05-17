10f5ae6a87cdca0165ffc01c110c6c5926a0956c
Linden Kyaw <31021147+tdslinden@users.noreply.github.com>
2022-05-16 20:54:29 -0700

    Time: 69 ms (66.48%), Space: 14.1 MB (53.72%) - LeetHub

--- 383-ransom-note/383-ransom-note.py
+++ 383-ransom-note/383-ransom-note.py
@@ -1,14 +1,3 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        map = {}
-        
-        for c in magazine:
-            map[c] = map.get(c, 0) + 1
-        
-        for c in ransomNote:
-            val = map.get(c, None)
-            if not val or val == 0:
-                return False
-            map[c] -= 1
-        
-        return True
+        return (collections.Counter(ransomNote) - collections.Counter(magazine)) == {}
