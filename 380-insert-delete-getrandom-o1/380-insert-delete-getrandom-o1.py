a4f0da43c174b72d217f907c28e4c5d19b820390
Linden Kyaw <31021147+tdslinden@users.noreply.github.com>
2022-05-17 00:23:19 -0700

    Time: 544 ms (55.68%), Space: 61.6 MB (15.00%) - LeetHub

--- 380-insert-delete-getrandom-o1/380-insert-delete-getrandom-o1.py
+++ 380-insert-delete-getrandom-o1/380-insert-delete-getrandom-o1.py
@@ -1,58 +1,44 @@
 class RandomizedSet:
 
     def __init__(self):
-        self.data_map = {} # dictionary, aka map, aka hashtable, aka hashmap
-        self.data = [] # list aka array
-
-    def insert(self, val: int) -> bool:
-
-        # the problem indicates we need to return False if the item 
-        # is already in the RandomizedSet---checking if it's in the
-        # dictionary is on average O(1) where as
-        # checking the array is on average O(n)
-        if val in self.data_map:
-            return False
-
-        # add the element to the dictionary. Setting the value as the 
-        # length of the list will accurately point to the index of the 
-        # new element. (len(some_list) is equal to the index of the last item +1)
-        self.data_map[val] = len(self.data)
-
-        # add to the list
-        self.data.append(val)
+        self.map = {} # this will hold kv pairs with the value as the key and the index of the value in the arr as the value
+        self.arr = [] # this will hold all the values
+        self.pos = 0 
         
-        return True
-
-    def remove(self, val: int) -> bool:
-
-        # again, if the item is not in the data_map, return False. 
-        # we check the dictionary instead of the list due to lookup complexity
-        if not val in self.data_map:
+    def insert(self, val: int) -> bool:
+        if val in self.map:
             return False
-
-        # essentially, we're going to move the last element in the list 
-        # into the location of the element we want to remove. 
-        # this is a significantly more efficient operation than the obvious 
-        # solution of removing the item and shifting the values of every item 
-        # in the dicitionary to match their new position in the list
-        last_elem_in_list = self.data[-1]
-        index_of_elem_to_remove = self.data_map[val]
-
-        self.data_map[last_elem_in_list] = index_of_elem_to_remove
-        self.data[index_of_elem_to_remove] = last_elem_in_list
-
-        # change the last element in the list to now be the value of the element 
-        # we want to remove
-        self.data[-1] = val
-
-        # remove the last element in the list
-        self.data.pop()
-
-        # remove the element to be removed from the dictionary
-        self.data_map.pop(val)
+        
+        self.arr.append(val)
+        self.map[val] = self.pos
+        self.pos += 1
+        return True
+        
+    def remove(self, val: int) -> bool:
+        if val not in self.map:
+            return False
+        
+        last_index = self.pos - 1
+        last_elem = self.arr[last_index]
+        
+        # get the index of the value we want to remove
+        index_to_remove = self.map[val] 
+        
+        self.arr[index_to_remove] = last_elem
+        self.map[last_elem] = index_to_remove
+        
+        self.map.pop(val)
+        self.arr.pop()
+        
+        self.pos -= 1
         return True
 
     def getRandom(self) -> int:
-        # if running outside of leetcode, you need to `import random`.
-        # random.choice will randomly select an element from the list of data.
-        return random.choice(self.data)
+        return random.choice(self.arr)
+
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
