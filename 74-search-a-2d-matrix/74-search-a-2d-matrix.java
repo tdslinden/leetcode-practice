5c80cbd4dfc5e44277e8b0b3219ae9c7fd46f21e
Linden Kyaw <31021147+tdslinden@users.noreply.github.com>
2022-05-29 22:12:36 -0700

    Time: 0 ms (100.00%), Space: 42.7 MB (53.65%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -1,8 +1,5 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        // use binary search on the first number of each row
-        // use binary search once the row is found
-        
         int[] arrToSearch = getArr(matrix, target);
         
         if (arrToSearch == null || arrToSearch.length == 0) {
@@ -10,14 +7,14 @@ class Solution {
         }
         
         int start = 0;
-        int end = arrToSearch.length;
+        int end = arrToSearch.length - 1;
         
-        while (start < end) {
+        while (start <= end) {
             int mid = (start + end) / 2;
             if (target == arrToSearch[mid]) {
                 return true;
             } else if (target < arrToSearch[mid]) {
-                end = mid;
+                end = mid - 1;
             } else {
                 start = mid  + 1;
             } 
@@ -28,14 +25,14 @@ class Solution {
     
     public int[] getArr(int[][] matrix, int target) {
         int start = 0;
-        int end = matrix.length;
+        int end = matrix.length - 1;
         
-        while (start < end) {
+        while (start <= end) {
             int mid = (start + end) / 2;
             if (target <= matrix[mid][matrix[mid].length-1] && target >= matrix[mid][0]) {
                 return matrix[mid];
             } else if (target < matrix[mid][0]) {
-                end = mid;
+                end = mid - 1;
             } else {
                 start = mid  + 1;
             }
