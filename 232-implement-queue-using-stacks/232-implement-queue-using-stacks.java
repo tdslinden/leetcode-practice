43646b82a9054559bf61aa4e5d20d33429def8fb
Linden Kyaw <31021147+tdslinden@users.noreply.github.com>
2022-06-06 22:02:54 -0700

    Time: 1 ms (56.62%), Space: 42 MB (29.03%) - LeetHub

---
+++ 232-implement-queue-using-stacks/232-implement-queue-using-stacks.java
@@ -0,0 +1,49 @@
+class MyQueue {
+    
+    private Deque<Integer> queued;
+    private Deque<Integer> ready;
+    
+    public MyQueue() {
+        queued = new ArrayDeque<>();
+        ready = new ArrayDeque<>();
+    }
+    
+    public void push(int x) {
+        if (ready.size() == 0 && queued.size() == 0) {
+            ready.push(x);
+        } else {
+            queued.push(x);   
+        }
+    }
+    
+    public int pop() {
+        if (ready.size() == 0 && queued.size() >= 1) {
+            while(queued.peek() != null) {
+                ready.push(queued.pop());
+            }
+        }
+        return ready.pop();
+    }
+    
+    public int peek() {
+        if (ready.size() == 0 && queued.size() >= 1) {
+            while(queued.peek() != null) {
+                ready.push(queued.pop());
+            }
+        }
+        return ready.peek();
+    }
+    
+    public boolean empty() {
+        return ready.size() == 0 && queued.size() == 0;
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
