b5b0431995374bf210b63fff4325433fbdd1175d
Linden Kyaw <31021147+tdslinden@users.noreply.github.com>
2022-06-14 22:57:58 -0700

    Time: 0 ms (100.00%), Space: 41.5 MB (57.74%) - LeetHub

--- 226-invert-binary-tree/226-invert-binary-tree.java
+++ 226-invert-binary-tree/226-invert-binary-tree.java
@@ -19,12 +19,17 @@ class Solution {
             return null;
         }
         
-        TreeNode left = invertTree(root.left);
-        TreeNode right = invertTree(root.right);
-
-        root.left = right;
-        root.right = left;
-
+        swap(root);
+        invertTree(root.left);
+        invertTree(root.right);
+            
         return root;
+        
+    }
+    
+    public void swap(TreeNode root) {
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
     }
 }
