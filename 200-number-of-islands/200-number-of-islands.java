58e263e71c363c21eba7e00e962a2a9f778b631d
Linden Kyaw <31021147+tdslinden@users.noreply.github.com>
2022-06-27 22:47:43 -0700

    Time: 4 ms (74.41%), Space: 57.3 MB (50.88%) - LeetHub

---
+++ 200-number-of-islands/200-number-of-islands.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        int count = 0;
+        int row = grid.length;
+        int col = grid[0].length;
+        int visited[][] = new int[row][col];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (grid[i][j] == '1' && visited[i][j] == 0) {
+                    dfs(grid, visited, i, j);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    
+    public void dfs (char[][] grid, int[][] visited, int row, int col) {
+        visited[row][col] = 1;
+        
+        // check top
+        if (row > 0 && grid[row-1][col] == '1' && visited[row-1][col] == 0) {
+            dfs(grid, visited, row-1, col);
+        }
+        
+        // check bottom
+        if (row + 1< grid.length && grid[row+1][col] == '1' && visited[row+1][col] == 0) {
+            dfs(grid, visited, row+1, col);
+        }
+        
+        // check right
+        if (col + 1 < grid[0].length && grid[row][col+1] == '1' && visited[row][col+1] == 0) {
+            dfs(grid, visited, row, col+1);
+        }        
+        
+        // check left
+        if (col > 0 && grid[row][col-1] == '1' && visited[row][col-1] == 0) {
+            dfs(grid, visited, row, col-1);
+        }        
+    }
+}
