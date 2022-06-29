e7a5d03ab04d1b40d90427b1bb20230b004c4be4
Linden Kyaw <31021147+tdslinden@users.noreply.github.com>
2022-06-28 22:12:39 -0700

    Time: 11 ms (33.01%), Space: 48.3 MB (12.27%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
@@ -10,52 +10,34 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        if (lists.length == 0 || lists == null) {
+        if (lists.length == 0) {
             return null;
         }
         
-        return divide(lists, 0, lists.length-1);
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
         
-    }
-    
-    public ListNode divide(ListNode[] lists, int start, int end) {
-        if (end - start == 0) {
-            return lists[start];
-        }
-        if (end - start == 1) {
-            return mergeLists(lists[start], lists[end]);
-        }
-        
-        int mid = start + ((end - start) / 2);
-        ListNode l1 = divide(lists, start, mid);
-        ListNode l2 = divide(lists, mid+1, end);
-        
-        return mergeLists(l1, l2);
-    }
-    
-    public ListNode mergeLists(ListNode list1, ListNode list2) {
-        ListNode head = new ListNode();
-        ListNode curr = head;
-            
-        while (list1 != null && list2 != null) {
-            if (list1.val < list2.val) {
-                curr.next = list1;
-                list1 = list1.next;
-            } else {
-                curr.next = list2;
-                list2 = list2.next;
+        for (ListNode list : lists) {
+            ListNode current = list;
+            while (current != null) {
+                pq.offer(current.val);
+                current = current.next;
             }
-            curr = curr.next;
         }
         
-        if (list1 != null) {
-            curr.next = list1;
+        if (pq.size() == 0) {
+            return null;
+        } 
+        return merge(pq);
+    }
+    
+    public ListNode merge(PriorityQueue<Integer> pq) {
+        ListNode head = new ListNode(pq.poll());
+        ListNode current = head;
+        while(pq.size() > 0) {
+            current.next = new ListNode(pq.poll());
+            current = current.next;
         }
         
-        if (list2 != null) {
-            curr.next = list2;
-        }
-        
-        return head.next;
+        return head;
     }
 }
