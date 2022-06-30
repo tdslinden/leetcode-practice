c7c6ee74384e6de21171421037ab7a53dcb6bc4b
Linden Kyaw <31021147+tdslinden@users.noreply.github.com>
2022-06-29 18:16:30 -0700

    Time: 35 ms (79.00%), Space: 118.2 MB (36.33%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
@@ -1,36 +1,22 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
-            @Override
-            public int compare(int[] a, int[] b) {
-
-                int aDistance = getDistance(a);
-                int bDistance = getDistance(b);
-                return bDistance - aDistance;
-            }
-        });
-        
-        int size = 0;
-        
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b)->(b[0]*b[0]+b[1]*b[1]-a[0]*a[0]-a[1]*a[1]));
         for (int[] point : points) {
-            pq.offer(point);
-            size++;
-            if (size > k) {
-                pq.poll();                
-                size--;
+            if(pq.size()>=k){
+                int[] maxPoint = pq.peek();
+                boolean further = compare(maxPoint, point);
+                if(further){
+                    pq.poll();
+                    pq.offer(point);
+                }
+            }else{
+                pq.offer(point);
             }
         }
-        
-        int[][] ans = new int[k][2];
-        int i = 0;
-        while (k-- > 0) {
-            ans[i++] = pq.poll();
-        }
-        
-        return ans;
+        return pq.toArray(new int[pq.size()][]);
     }
-    
-    private int getDistance(int [] point) {
-        return point[0] * point[0] + point[1] * point[1];
+
+    private boolean compare(int[] maxPoint, int[] point) {
+        return maxPoint[0]*maxPoint[0]+maxPoint[1]*maxPoint[1]-point[0]*point[0]-point[1]*point[1]>0;
     }
 }
