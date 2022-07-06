c15ed1a0fdf06fa908f1dfe695a89ff63d89d0f3
Linden Kyaw <31021147+tdslinden@users.noreply.github.com>
2022-07-05 22:17:09 -0700

    Time: 57 ms (54.65%), Space: 154.8 MB (18.84%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.java
+++ 239-sliding-window-maximum/239-sliding-window-maximum.java
@@ -1,23 +1,36 @@
-public class Solution {
+class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        int n = nums.length;
-        if (n == 0) {
-            return nums;
+        // 1. Use a deque to store the indexes and make sure head has the largest number
+        // 2. For each incoming element, we first check whether it's bigger than the nums[last element] of dq. If so, continue to remove nums[last element] of dq
+        // 3. Check each head element to see if they are >= (end - k + 1) range. If not, remove head
+        // 4. If end + 1 >= k, we update the result using nums[deque.peekFirst()] as head is always the big number's index
+        Deque<Integer> deque = new ArrayDeque<>();
+        int index = 0;
+        int[] result = new int[nums.length - k + 1];
+        
+        int left = 0;
+        int right = 0;
+        
+        while (right < nums.length) {
+            int cur = nums[right];
+            
+            while (!deque.isEmpty() && cur > nums[deque.peekLast()]) {
+                deque.removeLast();
+            }
+            
+            deque.addLast(right);
+            
+            if (left > deque.peekFirst()) {
+                deque.removeFirst();
+            }
+            
+            if (right + 1 >= k) { //
+                result[index++] = nums[deque.peekFirst()];
+                left++;
+            }
+            right++;
         }
-        int[] result = new int[n - k + 1];
-        LinkedList<Integer> dq = new LinkedList<>();
-        for (int i = 0; i < n; i++) {
-            if (!dq.isEmpty() && dq.peek() < i - k + 1) {
-                dq.poll();
-            }
-            while (!dq.isEmpty() && nums[i] >= nums[dq.peekLast()]) {
-                dq.pollLast();
-            }
-            dq.offer(i);
-            if (i - k + 1 >= 0) {
-                result[i - k + 1] = nums[dq.peek()];
-            }
-        }
+        
         return result;
     }
 }
