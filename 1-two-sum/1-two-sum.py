73c48093d9936562edf039584bc7377224f5cfd7
Linden Kyaw <31021147+tdslinden@users.noreply.github.com>
2022-07-06 19:52:28 -0700

    Time: 88 ms (70.03%), Space: 15.2 MB (24.34%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,21 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # can't use the same element twice
-        
         map = {}
-        ans = []
         
         for i in range(len(nums)):
             num = nums[i]
-            map[num] = map.get(num, i)
+            diff = target - num
             
-            difference = target - num
+            if diff in map:
+                return [i, map.get(diff)]
             
-            index = map.get(difference)
-            
-            if (index != None and index != i):
-                ans.append(i)
-                ans.append(index)
-                return ans
-            
-        return ans
+            map[num] = i
