55c13fd71db2d0145d1df3e0bf855e04f68bb4ad
Linden Kyaw <31021147+tdslinden@users.noreply.github.com>
2022-07-11 22:12:21 -0700

    Time: 903 ms (19.90%), Space: 519.9 MB (5.01%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        if (s.length() == 1) {
+            return true;
+        }
+        
+        // remove all non-alphanumeric characters
+        String clean = "";
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c >= '0' && c <= '9') {
+                clean += c;
+            } else if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')) {
+                clean += Character.toLowerCase(c);
+            }
+        }    
+        
+        System.out.println(clean);
+        
+        for (int f = 0, r = clean.length() - 1; f < r; f++, r--) {
+            char fc = clean.charAt(f);
+            char rc = clean.charAt(r);
+            
+            if (fc != rc) {
+                return false;
+            }
+        }
+            
+        return true;
+    }
+}
