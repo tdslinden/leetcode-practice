9aaea47e08de5c59245753968c914d9dcbc74052
Linden Kyaw <31021147+tdslinden@users.noreply.github.com>
2022-07-11 22:31:40 -0700

    Time: 6 ms (66.53%), Space: 43.2 MB (83.29%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -1,30 +1,26 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        if (s.length() == 1) {
-            return true;
-        }
-        
         s = s.toLowerCase();
+        int left = 0;
+        int right = s.length() - 1;
         
-        // remove all non-alphanumeric characters
-        String clean = "";
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9')) {
-                clean += c;
-            }
-        }    
-        
-        
-        for (int f = 0, r = clean.length() - 1; f < r; f++, r--) {
-            char fc = clean.charAt(f);
-            char rc = clean.charAt(r);
+        while (left < right) {
+            char cl = s.charAt(left);
+            char cr = s.charAt(right);
             
-            if (fc != rc) {
-                return false;
+            //if c1 is not valid then move forward
+            if (!Character.isLetterOrDigit(cl)) left++;
+            // if c2 is not valid move backward
+            else if (!Character.isLetterOrDigit(cr)) right--;
+            // otherwise compare
+            else{
+                if (cl != cr) return false;    
+                left++;
+                right--;    
             }
         }
-            
+        
+        
         return true;
     }
 }
