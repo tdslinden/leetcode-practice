47e1fc74af10e16a041c1c7e219e99824a4ff219
Linden Kyaw <31021147+tdslinden@users.noreply.github.com>
2022-07-13 19:51:39 -0700

    Time: 2 ms (34.09%), Space: 48 MB (57.37%) - LeetHub

--- 733-flood-fill/733-flood-fill.java
+++ 733-flood-fill/733-flood-fill.java
@@ -1,36 +1,27 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        if (image.length == 0 || image == null) {
-            return null;
-        }
-            
-        int oldColor = image[sr][sc];
+        int[][] directions = new int[][] {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
         
-        if (oldColor != color) {
-            dfs(image, sr, sc, color, oldColor);
+        // want to look for neighbours with this target
+        int target = image[sr][sc];
+        
+        if (target == color) {
+            return image;
+        }
+        
+        // set the src with the colour
+        image[sr][sc] = color;
+        
+        for (int i = 0; i < 4; i++) {
+            int[] direction = directions[i];
+            int r = sr + direction[0];
+            int c = sc + direction[1];
+            
+            if (r >= 0 && r < image.length && c >= 0 && c < image[0].length && image[r][c] == target) {
+                floodFill(image, r, c, color);
+            } 
         }
         
         return image;
-    }
-    
-    public void dfs(int[][] image, int row, int col, int color, int oldColor) {
-        if (image[row][col] == oldColor) {
-            image[row][col] = color;
-            if (row > 0) {
-                dfs(image, row-1, col, color, oldColor);
-            }
-
-            if (col > 0) {
-                dfs(image, row, col-1, color, oldColor);
-            }
-
-            if (col < image[0].length - 1) {
-                dfs(image, row, col+1, color, oldColor);
-            }
-
-            if (row < image.length - 1) {
-                dfs(image, row+1, col, color, oldColor);
-            }   
-        }
     }
 }
