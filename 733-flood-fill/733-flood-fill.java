0bcbddaa4b1885974ff06e486f702c56e5017b20
Linden Kyaw <31021147+tdslinden@users.noreply.github.com>
2022-07-14 20:40:08 -0700

    Time: 1 ms (90.40%), Space: 47.8 MB (70.57%) - LeetHub

--- 733-flood-fill/733-flood-fill.java
+++ 733-flood-fill/733-flood-fill.java
@@ -1,25 +1,23 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int[][] directions = new int[][] {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-        
-        // want to look for neighbours with this target
         int target = image[sr][sc];
         
         if (target == color) {
             return image;
         }
         
-        // set the src with the colour
         image[sr][sc] = color;
         
+        // right, down, left, up
+        int[] directions = {0, 1, 0, -1, 0};
+        
         for (int i = 0; i < 4; i++) {
-            int[] direction = directions[i];
-            int r = sr + direction[0];
-            int c = sc + direction[1];
+            int r = sr + directions[i];
+            int c = sc + directions[i+1];
             
             if (r >= 0 && r < image.length && c >= 0 && c < image[0].length && image[r][c] == target) {
-                floodFill(image, r, c, color);
-            } 
+                image = floodFill(image, r, c, color);
+            }
         }
         
         return image;
