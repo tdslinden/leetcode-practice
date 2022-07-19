ba1e5efba5a2de11ecedb7c78308eaa7a378b82e
Linden Kyaw <31021147+tdslinden@users.noreply.github.com>
2022-07-19 00:12:24 -0700

    Time: 0 ms (100.00%), Space: 41 MB (33.22%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int climbStairs(int n) {
+        if (n == 0 || n == 1 || n == 2) {
+            return n;
+        }
+        
+        int count = 0;
+        int prev = 1;
+        int curr = 2;
+        
+        for (int i = 3; i <= n; i++) {
+            count = prev + curr;
+            prev = curr;
+            curr = count;
+        }
+        
+        return count;
+    }
+}
