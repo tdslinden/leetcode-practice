69a35f9e95705b8e36250e25865c45d10c99856a
Linden Kyaw <31021147+tdslinden@users.noreply.github.com>
2022-07-18 23:39:09 -0700

    Time: 7 ms (57.05%), Space: 45.4 MB (76.57%) - LeetHub

--- 383-ransom-note/383-ransom-note.java
+++ 383-ransom-note/383-ransom-note.java
@@ -1,22 +1,18 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        HashMap<Character, Integer> map = new HashMap<>();
+        int[] arr = new int[255];
         
         // store magazine inside map
-        
         for (int i = 0; i < magazine.length(); i++) {
             char c = magazine.charAt(i);
-            map.put(c, map.getOrDefault(c, 0) + 1);
+            arr[c] = arr[c] + 1;
         }
         
         for (int i = 0; i < ransomNote.length(); i++) {
             char c = ransomNote.charAt(i);
             
-            if (map.containsKey(c)) {
-                if (map.get(c) == 0) {
-                    return false;
-                }
-                map.put(c, map.get(c) - 1);
+            if (arr[c] > 0) {
+                arr[c] = arr[c] - 1;
             } else {
                 return false;
             }
