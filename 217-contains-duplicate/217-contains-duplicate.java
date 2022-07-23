bd4ed7df3aabc965de0977eb941d557bd929a674
Linden Kyaw <31021147+tdslinden@users.noreply.github.com>
2022-07-22 21:44:55 -0700

    Time: 28 ms (15.68%), Space: 69.7 MB (47.00%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.java
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -1,11 +1,16 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
+        if (nums.length == 1) {
+            return false;
+        }
         
-        for (int i = 0, j = i + 1; j < nums.length; i++, j++) {
-            if (nums[i] == nums[j]) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for (int num : nums) {
+            if (map.containsKey(num)) {
                 return true;
             }
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
         
         return false;
